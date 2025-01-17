8e8290163ab9735e2d11da811c15820abc632e30
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-17 11:45:27 +0100

    Initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
+        <UserInformation posts={posts} />
+      </section>
+
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <SignedIn>
+          <PostForm />
+        </SignedIn>
+        <PostFeed posts={posts} />
+      </section>
+
+      <section className="hidden xl:inline justify-center col-span-2">
+        <Widget />
+      </section>
     </div>
-  )
+  );
 }
 
 export default Home
