de1583afb61974d20be066972e4f41aef7ae63a9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-17 11:46:00 +0100

    Initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import PostFeed from "@/components/PostFeed";
+import PostForm from "@/components/PostForm";
+import UserInformation from "@/components/UserInformation";
+import Widget from "@/components/Widget";
+import { Post } from "@/mongodb/models/post";
+import { SignedIn } from "@clerk/nextjs";
+import connectDB from "@/mongodb/db";
 
-const Home = () => {
+export const revalidate = 0;
+
+export default async function Home() {
+  await connectDB();
+  const posts = await Post.getAllPosts();
+
   return (
     <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
@@ -20,5 +31,3 @@ const Home = () => {
     </div>
   );
 }
-
-export default Home
