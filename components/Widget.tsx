a1118ee9034f621413937b2007c250dd2643c01b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-31 06:52:52 +0100

    Building the Widget component

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
-const Widget = () => {
+function Widget() {
   return (
-    <div>
-      <h1>Widget</h1>
+    <div className="ml-6 h-[790px]">
+      <iframe
+        src="https://www.linkedin.com/embed/feed/update/urn:li:share:7186102330092638208"
+        title="Embedded post"
+        className="w-fit 2xl:min-w-[400px] h-full"
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default Widget
+export default Widget;
